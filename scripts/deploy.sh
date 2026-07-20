e49f169875dc4c238830b41a16ee5c18dda35087
CloudSync Dev <dev@cloudsync.io>
2026-07-20 11:07:13 +0100

    chore(scan): periodic TMAS check, 2026-07-20

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,3 +37,4 @@ curl -X POST -H 'Content-type: application/json' \
 # Scan check: 2026-05-25T11:07:15 BST
 # Scan check: 2026-06-08T10:08:09 UTC
 # Scan check: 2026-06-22T10:10:48Z
+# Scan check: 2026-07-20T10:07:09Z
